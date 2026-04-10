919a8c648cfe382609dc7ec1dd372c0ce721e4fc
Seu Nome <seu@email.com>
2026-04-10 01:09:45 -0300

    Prepare deploy and clean project files

--- delete_photo.py
+++
@@ -1,6 +0,0 @@
-﻿import requests, json
-fotos = requests.get('http://localhost:5000/uploads').json()
-print('Deletando:', fotos[0]['id'])
-r = requests.delete(f'http://localhost:5000/uploads/{fotos[0]["id"]}')
-print('Status:', r.status_code)
-print('Resposta:', r.json())
